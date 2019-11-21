b7ba8f1cb8c177f1e4a9c489620d624a250028a4
BioMannequin <ashton-mills@outlook.com>
2019-11-21 11:33:31 +0000

    Created WavCaptain from the audio player project (not finished implementing gain slider yet, this code won't compile)

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,239 @@
+/*
+  ==============================================================================
+
+	This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+#define PLAYINIT "Play"
+
+
+//==============================================================================
+MainComponent::MainComponent() : openButton("Open"), playButton("Play"), stopButton("Stop"), state(Stopped),
+thumbnailCache(5), thumbnailComponent(512,formatManager,thumbnailCache), positionOverlay(transportSource),gain(0.5)
+{
+	// Make sure you set the size of the component after
+	// you add any child components.
+	setSize(600, 400);
+
+	openButton.onClick = [this] {openButtonClicked(); };
+	addAndMakeVisible(&openButton);
+
+	playButton.onClick = [this] {playButtonClicked(); };
+	addAndMakeVisible(&playButton);
+	playButton.setColour(TextButton::buttonColourId, Colours::green);
+	playButton.setEnabled(false);
+
+	stopButton.onClick = [this] {stopButtonClicked(); };
+	addAndMakeVisible(&stopButton);
+	stopButton.setColour(TextButton::buttonColourId, Colours::red);
+	stopButton.setEnabled(false);
+
+	transportSource.addChangeListener(this);
+	//transportSource.addChangeListener(this);
+
+	formatManager.registerBasicFormats();
+
+	addAndMakeVisible(thumbnailComponent);
+
+	addAndMakeVisible(positionOverlay);
+
+	addAndMakeVisible(gainSlider);
+	gainSlider.setSliderStyle(Slider::LinearVertical);
+	gainSlider.setRange(-1.0f, 1.0f, 0.01);
+	gainSlider.setValue(gain);
+	gainSlider.setTextBoxStyle(Slider::TextBoxBelow, true, 40, 30);
+
+
+
+	// Some platforms require permissions to open input channels so request that here
+	if (RuntimePermissions::isRequired(RuntimePermissions::recordAudio)
+		&& !RuntimePermissions::isGranted(RuntimePermissions::recordAudio))
+	{
+		RuntimePermissions::request(RuntimePermissions::recordAudio,
+			[&](bool granted) { if (granted)  setAudioChannels(2, 2); });
+	}
+	else
+	{
+		// Specify the number of input and output channels that we want to open
+		setAudioChannels(0, 2);
+	}
+}
+
+MainComponent::~MainComponent()
+{
+	// This shuts down the audio device and clears the audio source.
+	shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+	transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void MainComponent::getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill)
+{
+	if (playSource.get() == nullptr)
+	{
+		bufferToFill.clearActiveBufferRegion();
+		return;
+	}
+
+	transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void MainComponent::releaseResources()
+{
+	transportSource.releaseResources();
+}
+
+//==============================================================================
+void MainComponent::paint(Graphics& g)
+{
+	// (Our component is opaque, so we must completely fill the background with a solid colour)
+	g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
+
+	// You can add your drawing code here!
+//	Rectangle<int> thumbnailBounds(10, 100, getWidth() - 20, getHeight() - 120);
+
+}
+
+void MainComponent::resized()
+{
+	openButton.setBounds(10, 10, getWidth() - 20, 20);
+	playButton.setBounds(10, 40, getWidth() - 20, 20);
+	stopButton.setBounds(10, 70, getWidth() - 20, 20);
+
+	Rectangle<int>thumbnailBounds(10, 100, getWidth() - 60, getHeight() - 160);
+	thumbnailComponent.setBounds(thumbnailBounds);
+	positionOverlay.setBounds(thumbnailBounds);
+
+	gainSlider.setBounds(thumbnailBounds.getWidth() + 20, thumbnailBounds.getHeight()-150, 40, thumbnailBounds.getHeight()+10);
+}
+
+void MainComponent::openButtonClicked()
+{
+
+	//choose a file
+	FileChooser chooser("Choose a wav file",
+		{},
+		"*.wav");
+	//if the user chooses a file
+	if (chooser.browseForFileToOpen())
+	{
+		//what did the user choose? 
+		File myFile = chooser.getResult();
+		//read the file
+		AudioFormatReader* reader = formatManager.createReaderFor(myFile);
+		//get the file ready to play
+		if (reader != nullptr)
+		{
+			changeState(Stopped);
+			std::unique_ptr<AudioFormatReaderSource> newSource(new AudioFormatReaderSource(reader, true));
+			transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+			playButton.setEnabled(true);
+			playSource.reset(newSource.release());
+			playButton.setButtonText(PLAYINIT);
+			playButton.setColour(TextButton::buttonColourId, Colours::green);
+			thumbnailComponent.setFile(myFile);
+		}
+
+
+	}
+
+}
+
+void MainComponent::playButtonClicked()
+{
+	if ((state == Stopped) || (state == Paused))
+		changeState(Starting);
+	else
+		changeState(Pausing);
+}
+
+void MainComponent::stopButtonClicked()
+{
+	if (state == Paused)
+		changeState(Stopped);
+	else
+		changeState(Stopping);
+}
+
+
+void MainComponent::changeListenerCallback(ChangeBroadcaster* source)
+{
+	if (source == &transportSource)
+		transportSourceChanged();
+}
+
+
+void MainComponent::transportSourceChanged()
+{
+	if (transportSource.isPlaying())
+		changeState(Playing);
+	else if ((state == Playing) || (state == Stopping))
+		changeState(Stopped);
+	else if (state == Pausing)
+		changeState(Paused);
+}
+
+
+void MainComponent::changeState(TransportState newState)
+{
+	if (newState != state)
+	{
+		state = newState;
+
+		switch (state)
+		{
+		case Stopped:
+			stopButton.setEnabled(false);
+			playButton.setEnabled(true);
+			playButton.setButtonText(PLAYINIT);
+			playButton.setColour(TextButton::buttonColourId, Colours::green);
+			stopButton.setButtonText("Stop");
+			transportSource.setPosition(0.0);
+			break;
+
+		case Starting:
+			transportSource.start();
+			break;
+
+		case Playing:
+			stopButton.setEnabled(true);
+			playButton.setButtonText("Pause");
+			playButton.setColour(TextButton::buttonColourId, Colours::yellow);
+			stopButton.setButtonText("Stop");
+			break;
+
+		case Stopping:
+			transportSource.stop();
+
+			break;
+
+		case Pausing:
+			transportSource.stop();
+			break;
+
+		case Paused:
+			playButton.setButtonText("Resume");
+			playButton.setColour(TextButton::buttonColourId, Colours::green);
+			stopButton.setButtonText("Rewind");
+			break;
+		}
+	}
+}
+
+//try using the lambda version of this instead 
+
+void MainComponent::sliderValueChanged(Slider* slider)
+{
+	if (&gainSlider == slider)
+	{
+		gainSlider.set
+		transportSource.setGain(slider->getValue());
+	}
+}
---
+++ Source/MainComponent.h
@@ -0,0 +1,201 @@
+/*
+
+  ==============================================================================
+
+	This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+	This component lives inside our window, and this is where you should put all
+	your controls and content.
+*/
+
+// child component that controls what we paint in the thumnbnail box.
+//or telling us no file is loaded is there is no file opened. 
+
+class ThumbnailComponent : public Component, private ChangeListener
+{
+public : 
+	ThumbnailComponent(int sourceSamplesPerThumbnailSample,
+		AudioFormatManager& formatManager,
+		AudioThumbnailCache& thumbnailCache) : 
+		thumbnail (sourceSamplesPerThumbnailSample, formatManager,thumbnailCache)
+	{
+		thumbnail.addChangeListener(this);
+	}
+
+	void setFile(const File& file)
+	{
+		thumbnail.setSource(new FileInputSource(file));
+	}
+
+	void paint(Graphics& g) override
+	{
+		if (thumbnail.getNumChannels() == 0)
+			paintIfNoFileLoaded(g);
+		else
+			paintIfFileLoaded(g);
+	}
+
+	void paintIfNoFileLoaded(Graphics& g)
+	{
+		g.setColour(Colours::darkgrey);
+		g.fillAll();
+		g.setColour(Colours::white);
+		g.drawFittedText("No file loaded", getLocalBounds(), Justification::centred, 1.0f);
+	}
+
+	void paintIfFileLoaded(Graphics& g)
+	{
+		g.setColour(Colours::white);
+		g.fillAll();
+		g.setColour(Colours::darkgrey);
+
+		thumbnail.drawChannels(g,
+			getLocalBounds(),
+			0.0f,
+			thumbnail.getTotalLength(),
+			1.0f);
+		//g.setColour(Colours::black);
+		//auto audioPosition(transportSource.getCurrentPosition());
+		//auto drawPosition((audioPosition / audioLength) * thumbnailBounds.getWidth()
+		//	+ thumbnailBounds.getX());
+	//	g.drawLine(drawPosition, thumbnailBounds.getY(), drawPosition, thumbnailBounds.getBottom(), 2.0);
+	}
+
+	void changeListenerCallback(ChangeBroadcaster* source) override
+	{
+		if (source == &thumbnail)
+			thumbnailChanged();
+	}
+
+private:
+	void thumbnailChanged()
+	{
+		repaint();
+	}
+
+	AudioThumbnail thumbnail;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ThumbnailComponent)
+};
+
+//______________________________________________________________________________________________________________________
+
+//child component that handles the playback position line
+
+class PositionOverlay : public Component, public Timer
+{
+public:
+	PositionOverlay(AudioTransportSource& transportSourcetoUse)
+		: transportSource(transportSourcetoUse)
+	{
+		startTimer(40);
+	}
+
+	void paint(Graphics& g) override
+	{
+		auto duration = (float)transportSource.getLengthInSeconds();
+
+		if (duration > 0.0)
+		{
+			auto audioPosition = (float) transportSource.getCurrentPosition();  //why here do he have (float) in brackets? 
+			auto drawPosition =  (audioPosition / duration) * getWidth();
+			g.setColour(Colours::black);
+			g.drawLine(drawPosition, 0.0f, (float)drawPosition, getHeight(), 1.0);
+		}
+	}
+
+	void mouseDown(const MouseEvent& event) override
+	{
+		auto duration = transportSource.getLengthInSeconds();
+
+		if (duration > 0.0f)
+		{
+			auto clickPosition = event.position.x;
+			auto audioPosition = (clickPosition / getWidth()) * duration;
+
+			transportSource.setPosition(audioPosition);
+		}
+	}
+
+
+private:
+
+	void timerCallback()override
+	{
+		repaint();
+	}
+	AudioTransportSource& transportSource;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PositionOverlay)
+};
+
+class MainComponent : public AudioAppComponent, public ChangeListener, public Slider::Listener
+{
+public:
+	//==============================================================================
+	MainComponent();
+	~MainComponent();
+
+	//==============================================================================
+	void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+	void getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill) override;
+	void releaseResources() override;
+
+	//==============================================================================
+	void paint(Graphics& g) override;
+	void resized() override;
+
+private:
+	//==============================================================================
+	// Your private member variables go here...
+	enum TransportState
+	{
+		Stopped,
+		Starting,
+		Stopping,
+		Playing,
+		Pausing,
+		Paused
+	};
+
+	TransportState state;
+
+	void openButtonClicked();
+	void playButtonClicked();
+	void stopButtonClicked();
+
+	void transportSourceChanged();
+
+	void changeState(TransportState newState);
+	void changeListenerCallback(ChangeBroadcaster* source) override;
+
+	void sliderValueChanged(Slider* slider) override;
+
+	TextButton openButton;
+	TextButton playButton;
+	TextButton stopButton;
+
+	AudioTransportSource transportSource;
+
+	AudioFormatManager formatManager;
+	std::unique_ptr<AudioFormatReaderSource> playSource;
+
+	AudioThumbnailCache thumbnailCache;
+
+	ThumbnailComponent thumbnailComponent;
+	PositionOverlay positionOverlay;
+
+	Slider gainSlider;
+	float gain;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+};
